45f3db894546d9bc356667a096856512bbdbc07a
Nawar El Zein <nawarelzein@gmail.com>
2025-06-05 19:20:01 +0300

    Update DBConnection.java

--- src/main/java/clinicoop2/DBConnection.java
+++ src/main/java/clinicoop2/DBConnection.java
@@ -5,9 +5,9 @@ import java.sql.DriverManager;
 import java.sql.SQLException;
 
 public class DBConnection {
-    private static final String URL = "jdbc:mysql://localhost:3306/clinic2_db";
-    private static final String USER = "root";
-    private static final String PASSWORD = "Nawar<<123";
+    private static final String URL = DB-CONNECTION;
+    private static final String USER = USER;
+    private static final String PASSWORD = PASS;
 
     public static Connection getConnection() throws SQLException {
         return DriverManager.getConnection(URL, USER, PASSWORD);
